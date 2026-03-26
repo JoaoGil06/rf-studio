68e6c1149d2676770cae48635fecfaebe78c5ed2
João Gil <33355337+JoaoGil06@users.noreply.github.com>
2026-03-26 15:37:45 +0000

    feat: add register user repository (#8)

---
+++ src/domain/repository/user-repository.interface.ts
@@ -0,0 +1,6 @@
+import { User } from '../entity/user/user.entity.js';
+
+export interface IUserRepository {
+  findByEmail(email: string): Promise<User | null>;
+  save(user: User): Promise<void>;
+}
