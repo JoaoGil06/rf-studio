3e0c3bee9a4d4ca5c6d3741873d5adb42f204c4c
JoaoGil06 <joaogmonteiro06@gmail.com>
2026-08-03 17:12:36 +0100

    feat: testing CI with an error

--- backend/src/infrastructure/graphql/resolvers/index.ts
+++ backend/src/infrastructure/graphql/resolvers/index.ts
@@ -1,6 +1,5 @@
 import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs';
 import { mergeResolvers } from '@graphql-tools/merge';
-import { queryResolvers } from './queries/index.js';
 import { mutationResolvers } from './mutations/index.js';
 import { fieldResolvers } from './field_resolvers/index.js';
 
